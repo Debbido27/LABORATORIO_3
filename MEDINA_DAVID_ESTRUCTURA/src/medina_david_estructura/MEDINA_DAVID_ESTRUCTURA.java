f171e9e7b1326e7012d61fce751ca7350e289e62
Debbido27 <Davidmedina272004@gmail.com>
2024-11-01 09:07:53 -0600

    COMENTARIOS PARA UBICAR
    Use comentarios para poder ubicarme mejor en el codigo y desde el inicio ver donde ira cada funcione el main, y el menu con el do

---
+++ MEDINA_DAVID_ESTRUCTURA/src/medina_david_estructura/MEDINA_DAVID_ESTRUCTURA.java
@@ -0,0 +1,34 @@
+
+package medina_david_estructura;
+
+
+public class MEDINA_DAVID_ESTRUCTURA {
+//FUNCIONES PARA EL MENU
+    
+ // FUNCION PARA INVERTIR UNA PALABRA
+    
+    
+    
+    
+ // FUNCION  PARA EL NUMERO PERFECTO
+    
+    
+    
+ // FUNCION DE NUMERO ALEATORIO PRIMO
+    
+    
+    
+ // FUNCION PARA VOTACIONES
+    
+ 
+    
+  // FUNCION PARA CONTADOR DE OPCIONES
+ 
+  //MAIN
+    public static void main(String[] args) {
+      
+        //MENU PRINCIPAL DONDE SE LLAMARAN LAS FUNCIONES
+        
+    }
+    
+}
